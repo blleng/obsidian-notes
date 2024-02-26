a9824f7076c2d3c495ccbc172765b67d90f1e8f4
blleng <l0bl@outlook.com>
2024-02-27 07:38:47 +0800

    note icon

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,9 +6,8 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const icon = fileData.frontmatter?.noteIcon
   return (
-    <h1 class={classNames(displayClass, "page-title")} data-icon={icon}>
+    <h1 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
     </h1>
   )
